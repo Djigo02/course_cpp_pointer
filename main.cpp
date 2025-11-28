a21d12a3b78de0f1b363cf80f25432a3fade9d6e
Mouhamad DJIGO <mehmetdjigo@gmail.com>
2025-11-28 00:28:07 +0000

    pointeur et structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "my_function.cpp"
 
+namespace tab_pointeur{
 
+    void until_function(){
 
-int main()
-{
-    // tableau de 5 elements
+// tableau de 5 elements
     int tab[5] = {1,2,3,4,5};
 
 
@@ -28,12 +28,60 @@ int main()
     }
     cout << endl;
 
+    cout << "------------------------" << endl;
 
+    }
+}
 
+typedef struct Personne * PPersonne;
 
+typedef struct Personne{
+    string prenom, nom;
+    int age;
+};
 
+int main()
+{
+    // declarer le pointeur sur une personne et pointee nul part
+    Personne * ptr_pers = nullptr;
+    Personne p ;
 
+    cout << "Donnez le nom : " << endl;
+    cin >> p.nom;
 
+    cout << "Donnez le prenom : " << endl;
+    cin >> p.prenom;
+
+    cout << "Donnez l'age : " << endl;
+    cin >> p.age;
+
+    //! Affichage avec la variable
+    cout << "Prenom : " << p.prenom << " Nom : " << p.nom <<
+        " age : " << p.age << endl;
+    // pointez vers la variable personne
+    ptr_pers = &p;
+
+    //! mode d'acces
+    //! nom_pointeur->nom_du_champs
+    //string prenom = ptr_pers->prenom;
+
+    //! Affichage avec le pointeur
+    cout << "Prenom : " << ptr_pers->prenom << " Nom : " << ptr_pers->nom <<
+        " age : " << ptr_pers->age;
+
+    //! Allocation dynamique
+    PPersonne pp = new Personne;
+
+    cout << "Donnez le nom : " << endl;
+    cin >> pp->nom;
+    cout << "Donnez le prenom : " << endl;
+    cin >> pp->prenom;
+    cout << "Donnez l'age : " << endl;
+    cin >> pp->age;
+
+    //! Affichage avec le pointeur
+    cout << "Prenom : " << pp->prenom << " Nom : " << pp->nom <<
+        " age : " << pp->age;
 
 
 
